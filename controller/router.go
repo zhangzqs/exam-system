cc1f9cbb9054230dbeea99ede44525fd1291215d
zzq <i@zhangzqs.cn>
2022-06-13 16:55:55 +0800

    add: 添加试卷管理相关路由

--- controller/router.go
+++ controller/router.go
@@ -11,14 +11,20 @@ func InitRouter(r *gin.Engine) {
 		sessionGroup.POST("/login", Login)
 		sessionGroup.POST("/register", Register)
 	}
-	questionsGroup := r.Group("/questions")
-	questionsGroup.Use(JwtAuthMiddleware)
+	questionsGroup := r.Group("/questions", JwtAuthMiddleware)
 	{
 		questionsGroup.POST("/", AddQuestion)
+		questionsGroup.GET("/", GetUserQuestions)
 		questionsGroup.DELETE("/:id", DeleteQuestion)
 		questionsGroup.PUT("/:id", UpdateQuestion)
 		questionsGroup.GET("/:id", GetQuestion)
 	}
-	r.GET("/users/:uid/questions", GetUserQuestions)
-
+	papersGroup := r.Group("/papers", JwtAuthMiddleware)
+	{
+		papersGroup.POST("/")
+		papersGroup.GET("/")
+		papersGroup.DELETE("/:id")
+		papersGroup.PUT("/:id")
+		papersGroup.GET("/:id")
+	}
 }
